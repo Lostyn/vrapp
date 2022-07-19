cf3944bfce157746c57e7ad47c3700318b347379
Vincent <vincent@hyperfiction.fr>
2022-07-19 12:09:22 +0200

    clean old build pipe

--- gulpfile.ts/_application/appScripts.ts
+++
@@ -1,9 +0,0 @@
-// import gulp from 'gulp';
-// import paths from '../gulp.path';
-// import babel from "gulp-babel";
-
-// export default function appScripts() {
-// 	return gulp.src(paths.app.scripts)
-// 		.pipe(babel())
-// 		.pipe(gulp.dest(paths.app.buildDir));
-// }
